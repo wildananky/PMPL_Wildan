502a0fa9c79d1abf07aec2b92cf1fb7ce71cc84b
wildananky <wildan.anky@icloud.com>
2015-09-09 00:31:49 +0700

    first test failed yep yep

--- functional-test.py
+++ functional-test.py
@@ -1,6 +1,27 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase): #1
 
-assert 'Django' in browser.title
+	def setUp(self): #2
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self): #3
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self): #4
+		# edith menemukan web baru dan mencoba blah blah
+		self.browser.get('http://localhost:8000')
+		
+		# dia sadar page titlenya itu nyebut to-do list
+		self.assertIn('To-Do', self.browser.title) #5
+		self.fail('Finish the test') #6
+
+		# dia boleh memasukan item setelahnya
+		# dan blah blah blah
+		# more blah blah blah
+		# blah blah blah blah
+
+if __name__ == '__main__': #7
+	unittest.main(warnings='ignore') #8
