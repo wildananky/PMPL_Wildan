e552e635f83df3369a9bbae514270617202bd4c2
wildananky <wildan.anky@icloud.com>
2015-09-09 17:14:58 +0700

    Functional test now checks we can input a to-do item

--- functional-test.py
+++ functional-test.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(10)
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -15,10 +16,31 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 		
 		# dia sadar page titlenya itu nyebut to-do list
-		self.assertIn('Web Pribadi', self.browser.title)
+		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('Web Pribadi Wildan Anky', header_text)
+		self.assertIn('To-Do', header_text)
 
+		# dia memasukan to-do item satu per satu
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+
+		# dia mengetikan "Buy Peacock feathers" pada textbox
+		inputbox.send_keys('Buy peacock feathers')
+		
+		# saat dia menekan ENTER pagenya diupdate
+		# "1: Buy peacock feathers" sebagai item pada to-do list
+		inputbox.send_keys(Keys.ENTER)
+		
+		table = self.browser.find_elements_by_tag_name('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers') for row in rows
+		)
+
+		self.fail('Finish the test!')
 		# dia boleh memasukan item setelahnya
 		# dan blah blah blah
 		# more blah blah blah
