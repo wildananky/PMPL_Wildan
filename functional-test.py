0246fef75dd1542de18b0a00e97cf11ee4bb024f
wildananky <wildan.anky@icloud.com>
2015-09-09 14:27:00 +0700

    Refractor home page view to use a template

--- functional-test.py
+++ functional-test.py
@@ -1,22 +1,24 @@
 from selenium import webdriver
 import unittest
 
-class NewVisitorTest(unittest.TestCase): #1
+class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self): #2
+	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
-	def tearDown(self): #3
+	def tearDown(self):
 		self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self): #4
+	def test_can_start_a_list_and_retrieve_it_later(self):
 		# edith menemukan web baru dan mencoba blah blah
 		self.browser.get('http://localhost:8000')
 		
 		# dia sadar page titlenya itu nyebut to-do list
-		self.assertIn('To-Do lists', self.browser.title) #5
-		self.fail('Finish the test') #6
+		self.assertIn('To-Do lists', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Web Pribadi Wildan Anky', header_text)
+		self.fail('Finish the test')
 
 		# dia boleh memasukan item setelahnya
 		# dan blah blah blah
