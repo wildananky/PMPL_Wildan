306e624fdd4c4ef5bd468f478db3bedc1d67b2a3
wildananky <wildan.anky@icloud.com>
2015-09-10 10:49:43 +0700

    sudah berhasil POST tapi belum menyimpan

--- functional-test.py
+++ functional-test.py
@@ -33,19 +33,29 @@ class NewVisitorTest(unittest.TestCase):
 		# saat dia menekan ENTER pagenya diupdate
 		# "1: Buy peacock feathers" sebagai item pada to-do list
 		inputbox.send_keys(Keys.ENTER)
-		
+
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
+	
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+	
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly', [row.text for row in rows]
 		)
 
-		self.fail('Finish the test!')
 		# dia boleh memasukan item setelahnya
 		# dan blah blah blah
 		# more blah blah blah
 		# blah blah blah blah
+		self.fail('Finish the test!')
 
 if __name__ == '__main__': #7
 	unittest.main(warnings='ignore') #8
