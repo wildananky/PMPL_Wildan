71fe84c176851ab939d1b6d9deff1fc8ee477b6c
wildananky <wildan.anky@icloud.com>
2015-09-23 18:05:50 +0700

    Next To: One More View to Handle Adding Items to an Existing List

--- lists/tests.py
+++ lists/tests.py
@@ -57,18 +57,24 @@ class ListAndItemModelTest(TestCase):
 class ListViewTest(TestCase):
 
 	def test_uses_list_template(self):
-		response = self.client.get('/lists/the-only-list-in-the-world')
-		self.assertTemplateUsed('list.html')
-
-	def test_displays_all_items(self):
 		list_ = List.objects.create()
-		Item.objects.create(text='itemey 1', list=list_)
-		Item.objects.create(text='itemey 2', list=list_)
+		response = self.client.get('/lists/%d/' % (list_.id))
+		self.assertTemplateUsed(response, 'list.html')
 
-		response = self.client.get('/lists/the-only-list-in-the-world/')
+	def test_displays_only_items_for_that_list(self):
+		correct_list = List.objects.create()
+		Item.objects.create(text='itemey 1', list=correct_list)
+		Item.objects.create(text='itemey 2', list=correct_list)
+		other_list = List.objects.create()
+		Item.objects.create(text='other list item 1', list=other_list)
+		Item.objects.create(text='other list item 2', list=other_list)
+
+		response = self.client.get('/lists/%d/' % (correct_list.id,))
 
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
+		self.assertNotContains(response, 'other list item 1')
+		self.assertNotContains(response, 'other list item 2')
 
 class NewListTest(TestCase):
 
@@ -87,4 +93,5 @@ class NewListTest(TestCase):
 			'/lists/new',
 			data={'item_text': 'A new list item'}
 		)
-		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+		new_list = List.objects.first()
+		self.assertRedirects(response, '/lists/%d/' % (new_list.id))
