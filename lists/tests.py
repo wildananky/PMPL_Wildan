e220bf7324d98418e8a363ecc1eaeef6e21658bc
wildananky <wildan.anky@icloud.com>
2015-09-23 16:22:44 +0700

    new URL, view and template to display list

--- lists/tests.py
+++ lists/tests.py
@@ -106,7 +106,7 @@ class ListViewTest(TestCase):
 
 	def test_uses_list_template(self):
 		response = self.client.get('/lists/the-only-list-in-the-world')
-		self.assertTemplateUsed(response, 'list.html')
+		self.assertTemplateUsed('list.html')
 
 	def test_displays_all_items(self):
 		Item.objects.create(text='itemey 1')
